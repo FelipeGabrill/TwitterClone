c50202bd98f5e4fa3fc6dde8c079cb2f59891332
Felipe Gabriel <felipegabrielodss@gmail.com>
2026-01-27 20:14:44 -0300

    feat(user-service): add user domain layer

---
+++ user-service/src/main/java/com/felipegabrill/twitter/user_service/domain/user/User.java
@@ -0,0 +1,171 @@
+package com.felipegabrill.twitter.user_service.domain.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class User {
+
+    private UUID id;
+
+    private String username;
+
+    private String name;
+    private String bio;
+    private String profileImageUrl;
+    private String bannerImageUrl;
+
+    private String location;
+
+    private Integer followersCount;
+    private Integer followingCount;
+    private Integer tweetsCount;
+
+    private Boolean isPrivate;
+    private Boolean active;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public User() {
+    }
+
+    public User(
+            UUID id,
+            String username,
+            String name,
+            String bio,
+            String location,
+            String profileImageUrl,
+            String bannerImageUrl
+    ) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.bio = bio;
+        this.location = location;
+        this.profileImageUrl = profileImageUrl;
+        this.bannerImageUrl = bannerImageUrl;
+
+        this.followersCount = 0;
+        this.followingCount = 0;
+        this.tweetsCount = 0;
+
+        this.active = true;
+        this.isPrivate = false;
+
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getBannerImageUrl() {
+        return bannerImageUrl;
+    }
+
+    public void setBannerImageUrl(String bannerImageUrl) {
+        this.bannerImageUrl = bannerImageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public Integer getFollowersCount() {
+        return followersCount;
+    }
+
+    public void setFollowersCount(Integer followersCount) {
+        this.followersCount = followersCount;
+    }
+
+    public Integer getTweetsCount() {
+        return tweetsCount;
+    }
+
+    public void setTweetsCount(Integer tweetsCount) {
+        this.tweetsCount = tweetsCount;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getFollowingCount() {
+        return followingCount;
+    }
+
+    public void setFollowingCount(Integer followingCount) {
+        this.followingCount = followingCount;
+    }
+
+    public Boolean isPrivate() {
+        return isPrivate;
+    }
+
+    public void setPrivate(Boolean isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+
+    public Boolean isActive() {
+        return active;
+    }
+
+    public void setActive(Boolean isActive) {
+        this.active = isActive;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
