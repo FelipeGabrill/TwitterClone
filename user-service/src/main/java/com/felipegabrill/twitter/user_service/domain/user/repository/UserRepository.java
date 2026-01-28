1100ba11f77088bdb08f4b243563eebd41fbf552
Felipe Gabriel <felipegabrielodss@gmail.com>
2026-01-27 22:29:47 -0300

    feat(user-service): add methods to UserRepository

--- user-service/src/main/java/com/felipegabrill/twitter/user_service/domain/user/repository/UserRepository.java
+++ user-service/src/main/java/com/felipegabrill/twitter/user_service/domain/user/repository/UserRepository.java
@@ -1,4 +1,17 @@
 package com.felipegabrill.twitter.user_service.domain.user.repository;
 
-public class UserRepository {
+import com.felipegabrill.twitter.user_service.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+
+    User save(User user);
+    Optional<User> findById(UUID id);
+    Optional<User> findByUsername(String username);
+    Page<User> findByUsernameContainingIgnoreCaseOrNameContainingIgnoreCase(String username, String name, Pageable pageable);
+
 }
