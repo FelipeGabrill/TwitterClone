d737eb2171a82eb5498eae2cbe54f8bf3e1b6068
Felipe Gabriel <felipegabrielodss@gmail.com>
2026-01-27 22:31:23 -0300

    feat(user-service): add UserMapper to map domain and JPA entity

--- user-service/src/main/java/com/felipegabrill/twitter/user_service/infrastructure/mappers/UserMapper.java
+++ user-service/src/main/java/com/felipegabrill/twitter/user_service/infrastructure/mappers/UserMapper.java
@@ -1,4 +1,14 @@
 package com.felipegabrill.twitter.user_service.infrastructure.mappers;
 
-public class UserMapper {
+import com.felipegabrill.twitter.user_service.adapters.outbound.entities.JpaUserEntity;
+import com.felipegabrill.twitter.user_service.domain.user.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    User toDomain(JpaUserEntity entity);
+
+    JpaUserEntity toEntity(User domain);
+
 }
