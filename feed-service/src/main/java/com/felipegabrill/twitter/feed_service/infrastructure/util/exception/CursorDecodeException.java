bc354d96576980ae875a0f1b04573e7cbc1bf221
Felipe Gabriel <felipegabrielodss@gmail.com>
2026-02-06 03:05:24 -0300

    feat(feed-service): add cursor encode/decode exceptions

---
+++ feed-service/src/main/java/com/felipegabrill/twitter/feed_service/infrastructure/util/exception/CursorDecodeException.java
@@ -0,0 +1,7 @@
+package com.felipegabrill.twitter.feed_service.infrastructure.util.exception;
+
+public class CursorDecodeException extends RuntimeException {
+    public CursorDecodeException(String message) {
+        super(message);
+    }
+}
---
+++ feed-service/src/main/java/com/felipegabrill/twitter/feed_service/infrastructure/util/exception/CursorEncodeException.java
@@ -0,0 +1,7 @@
+package com.felipegabrill.twitter.feed_service.infrastructure.util.exception;
+
+public class CursorEncodeException extends RuntimeException {
+    public CursorEncodeException(String message) {
+        super(message);
+    }
+}
