d4e0d1d5bab1abb9da95d56ef58459d333d07c5c
Felipe Gabriel <felipegabrielodss@gmail.com>
2026-02-06 02:30:53 -0300

    chore(feed-service): bootstrap application and dependencies

--- feed-service/src/main/java/com/felipegabrill/twitter/feed_service/FeedServiceApplication.java
+++ feed-service/src/main/java/com/felipegabrill/twitter/feed_service/FeedServiceApplication.java
@@ -3,7 +3,9 @@ package com.felipegabrill.twitter.feed_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        io.awspring.cloud.autoconfigure.dynamodb.DynamoDbAutoConfiguration.class
+})
 public class FeedServiceApplication {
 
 	public static void main(String[] args) {
