b62b49f99cffb9f097aea48cba4b6a5d1699f08d
Felipe Gabriel <felipegabrielodss@gmail.com>
2026-02-06 02:31:13 -0300

    feat(feed-service): configure AWS DynamoDB and environment variables

---
+++ feed-service/src/main/java/com/felipegabrill/twitter/feed_service/infrastructure/config/aws/DynamoDBEnhancedConfig.java
@@ -0,0 +1,38 @@
+package com.felipegabrill.twitter.feed_service.infrastructure.config.aws;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
+public class DynamoDBEnhancedConfig {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhanced() {
+
+        DynamoDbClient dynamoDbClient = DynamoDbClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
+                .build();
+
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient)
+                .build();
+    }
+}
