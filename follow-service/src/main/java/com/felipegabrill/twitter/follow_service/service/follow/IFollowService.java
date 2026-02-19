f7af77751b412a682e97b35d0fd342bd39411e1d
Felipe Gabriel <felipegabrielodss@gmail.com>
2026-02-19 01:36:56 -0300

    docs(follow-service): add JavaDoc to service interface

--- follow-service/src/main/java/com/felipegabrill/twitter/follow_service/service/follow/IFollowService.java
+++ follow-service/src/main/java/com/felipegabrill/twitter/follow_service/service/follow/IFollowService.java
@@ -8,14 +8,48 @@ import java.util.UUID;
 
 public interface IFollowService {
 
+    /**
+     * Follows a user on behalf of another user.
+     *
+     * @param followerId  the ID of the user who wants to follow
+     * @param followingId the ID of the user to be followed
+     * @return a FollowResponseDTO representing the follow relationship
+     */
     FollowResponseDTO followUser(UUID followerId, UUID followingId);
 
+    /**
+     * Unfollows a user on behalf of another user.
+     *
+     * @param followerId  the ID of the user who wants to unfollow
+     * @param followingId the ID of the user to be unfollowed
+     */
     void unfollowUser(UUID followerId, UUID followingId);
 
+    /**
+     * Retrieves a paginated list of users that a given user is following.
+     *
+     * @param followerId the ID of the user whose following list is being retrieved
+     * @param pageable   pagination information
+     * @return a page of FollowResponseDTO representing the users being followed
+     */
     Page<FollowResponseDTO> getFollowing(UUID followerId, Pageable pageable);
 
+    /**
+     * Retrieves a paginated list of followers for a given user.
+     *
+     * @param followingId the ID of the user whose followers are being retrieved
+     * @param pageable    pagination information
+     * @return a page of FollowResponseDTO representing the followers
+     */
     Page<FollowResponseDTO> getFollowers(UUID followingId, Pageable pageable);
 
+    /**
+     * Checks if a user is following another user.
+     *
+     * @param followerId  the ID of the user who might be following
+     * @param followingId the ID of the user who might be followed
+     * @return true if followerId follows followingId, false otherwise
+     */
     boolean isFollowing(UUID followerId, UUID followingId);
-
 }
+
