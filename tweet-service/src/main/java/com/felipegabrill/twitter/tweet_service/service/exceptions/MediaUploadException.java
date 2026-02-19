00157466c4c90756a95cd1ba8868761795f02055
Felipe Gabriel <felipegabrielodss@gmail.com>
2026-02-19 01:51:06 -0300

    refactor(tweet-service): restructure and centralize service exceptions

--- tweet-service/src/main/java/com/felipegabrill/twitter/tweet_service/service/exceptions/MediaUploadException.java
+++ tweet-service/src/main/java/com/felipegabrill/twitter/tweet_service/service/exceptions/MediaUploadException.java
@@ -1,4 +1,4 @@
-package com.felipegabrill.twitter.tweet_service.service.tweet.exceptions;
+package com.felipegabrill.twitter.tweet_service.service.exceptions;
 
 public class MediaUploadException extends RuntimeException {
 
--- tweet-service/src/main/java/com/felipegabrill/twitter/tweet_service/service/exceptions/UnknownTweetTypeException.java
+++ tweet-service/src/main/java/com/felipegabrill/twitter/tweet_service/service/exceptions/UnknownTweetTypeException.java
@@ -1,4 +1,4 @@
-package com.felipegabrill.twitter.tweet_service.service.tweet.exceptions;
+package com.felipegabrill.twitter.tweet_service.service.exceptions;
 
 public class UnknownTweetTypeException extends RuntimeException {
     public UnknownTweetTypeException(String message) {
