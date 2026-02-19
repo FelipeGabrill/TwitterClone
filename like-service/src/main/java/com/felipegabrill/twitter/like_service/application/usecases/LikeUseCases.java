10251ef434ea94f672d311200f063cc0fc73f5f8
Felipe Gabriel <felipegabrielodss@gmail.com>
2026-02-19 01:33:37 -0300

    docs(like-service): add JavaDoc to service interfaces

--- like-service/src/main/java/com/felipegabrill/twitter/like_service/application/usecases/LikeUseCases.java
+++ like-service/src/main/java/com/felipegabrill/twitter/like_service/application/usecases/LikeUseCases.java
@@ -9,12 +9,38 @@ import java.util.UUID;
 
 public interface LikeUseCases {
 
+    /**
+     * Likes a tweet on behalf of a user.
+     *
+     * @param userId  the ID of the user who is liking the tweet
+     * @param tweetId the ID of the tweet to like
+     */
     void like(UUID userId, UUID tweetId);
 
+    /**
+     * Removes a like from a tweet on behalf of a user.
+     *
+     * @param userId  the ID of the user who is unliking the tweet
+     * @param tweetId the ID of the tweet to unlike
+     */
     void unlike(UUID userId, UUID tweetId);
 
+    /**
+     * Checks if a user has liked a specific tweet.
+     *
+     * @param userId  the ID of the user
+     * @param tweetId the ID of the tweet
+     * @return a LikeStatusResponseDTO indicating whether the user has liked the tweet
+     */
     LikeStatusResponseDTO hasLiked(UUID userId, UUID tweetId);
 
+    /**
+     * Retrieves a paginated list of likes for a given tweet.
+     *
+     * @param tweetId  the ID of the tweet
+     * @param pageable pagination information
+     * @return a page of LikeResponseDTO representing the likes on the tweet
+     */
     Page<LikeResponseDTO> listLikesByTweetId(UUID tweetId, Pageable pageable);
-
 }
+
